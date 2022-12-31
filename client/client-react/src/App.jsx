557c051f8b273870745c4a295a04e6a43a35b72e
SARB <mrsinghbusiness05@gmail.com>
2022-12-31 08:31:06 +0530

    DOne

---
+++ client/client-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+ 
+  return (
+    <div className="App">
+          <div className="main">
+              <h2> INstal  </h2>
+          </div>
+    </div>
+  )
+}
+
+export default App
