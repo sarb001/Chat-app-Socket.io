b063415f4e2a1c971ac63cc9feb77c112d161f9c
SARB <mrsinghbusiness05@gmail.com>
2023-01-01 11:16:25 +0530

    feature - User JOINED Done

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,6 @@ const io = new Server({
     cors : true,
 });
 const app = express();
-
 app.use(bodyParser.json());
 
 const emailtosocketmapping = new  Map();
@@ -16,13 +15,12 @@ const emailtosocketmapping = new  Map();
 io.on('connection' , (socket) => {
 
     console.log(' New Connection ');
-
     socket.on('join-room' , (data) => {
         const { roomid , emailid } = data ;
         console.log('User' , emailid , 'Joined Room' ,roomid );
         emailtosocketmapping.set(emailid ,socket.id);
         socket.join(roomid);
-                             // on Backend  side - Joined me in this Room  ( Below )
+                             // on Backend Event Trigerred  Side - Joined me in this Room  ( Below )
         socket.emit('joined-room' , {roomid});          
         socket.broadcast.to(roomid).emit('user-joined' , { emailid });
     });
